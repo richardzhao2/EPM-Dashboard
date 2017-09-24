ac98fa934d136705658be3c5ee05301044efaf1a
mikinty <myou@andrew.cmu.edu>
2017-09-24 04:16:15 -0400

    got the server running

--- App/server/serve.py
+++ App/server/serve.py
@@ -9,42 +9,65 @@ import pandas as pd
 
 app = Flask(__name__)
 
+global shot_df
+
+dir = ''
+
 names = {
-  2544: 'LeBron James',
-2747: 'JR Smith',
-201567: 'Kevin Love',
-202681: 'Kyrie Irving',
-202684: 'Tristan Thompson',
-2546: 'Carmelo Anthony',
-201149: 'Joakim Noah',
-201584: 'Courtney Lee',
-201565: 'Derrick Rose',
-204001: 'Kristaps Porzingis',
+  3704: 'LeBron James',
+	3835: 'JR Smith',
+	4391: 'Kevin Love',
+	4840: 'Kyrie Irving',
+	4884: 'Tristan Thompson',
+	3706: 'Carmelo Anthony',
+	4287: 'Joakim Noah',
+	4484: 'Courtney Lee',
+	4645: 'Derrick Rose',
+	5464: 'Kristaps Porzingis',
 }
 
 def find_shot_df(rdata):
-	game_indices = set(shot_df.index[df['GAME_ID'] == rdata['gameID']].tolist())
-	time_indices = set(shot_df.index[df['GAME_CLOCK'] == rdata['time']].tolist())
-	quarter_indices = set(shot_df.index[df['PERIOD'] == rdata['quarter']].tolist())
-	index = list(game_indices & time_indices & quarter_indices)[0]
+	game_indices = set(shot_df.index[shot_df['SV_GAME_ID'] == rdata['gameID']].tolist())
+	time_indices = set(shot_df.index[abs((shot_df['GAME_CLOCK'] /10) - int(float(rdata['time']))) < 5].tolist())
+	quarter_indices = set(shot_df.index[shot_df['PERIOD'] == rdata['quarter']].tolist())
+	print(len(game_indices))
+	print(len(time_indices))
+	print(len(quarter_indices))
+	common = game_indices & time_indices & quarter_indices
+	if (len(common) < 1):
+		print('no entry found')
+		return
+
+	index = list(game_indices & time_indices & quarter_indices)
+
+	print(index)
+
+	index = index[0]
+
+	# print('ASDFLAKSDKJFLASDJFALSDKFJASLDKFJASLDKDFJAS')
+	row = shot_df.iloc[index]
 
 	# get the data needed for expected shot value
 
-	print('player id', shot_df[index, 5])
+	player = row['SV_PLAYER_ID']
 
-	return {'playerID': names[shot_df[index, 5]], 'pts_type': shot_df[index, 17],
-		'fgm': shot_df[index, 18], 'fga': shot_df[index, 19], 'pts': shot_df[index, 20], 'dribbles': shot_df[index, 11]}
+	if (player not in names.keys()):
+		name = 'nobody'
+		print('not famous', player)
+	else:
+		name = names[player]
+
+	return {'playerID': name, 'pts_type': str(row['PTS_TYPE']),
+		'fgm': str(row['FGM']), 'fga': str(row['FGA']), 'pts': str(row['PTS']), 'dribbles': str(row['DRIBBLES'])}
 
 def find_possess_df(rdata):
-	game_indices = set(possess_df.index[df['GAME_ID'] == rdata['gameID']].tolist())
-	time_indices = set(possess_df.index[df['GAME_CLOCK']/10 == rdata['time']].tolist())
-	quarter_indices = set(possess_df.index[df['PERIOD'] == rdata['quarter']].tolist())
+	game_indices = set(possess_df.index[possess_df['GAME_ID'] == rdata['gameID']].tolist())
+	time_indices = set(possess_df.index[possess_df['GAME_CLOCK']/10 == rdata['time']].tolist())
+	quarter_indices = set(possess_df.index[possess_df['PERIOD'] == rdata['quarter']].tolist())
 	index = list(game_indices & time_indices & quarter_indices)[0]
 
 	touches = possess_df[index, 11]
 	return touches
-
-
 
 CORS(app)
 
@@ -55,13 +78,15 @@ def index1():
 @app.route('/data', methods=['POST'])
 def index():
 	rdata = request.get_json() # { gameID: 3843, time: 3839, quarter: 29823434} 
-	print('received request')
+	print('received request', rdata)
 	return jsonify(find_shot_df(rdata))
 
 def startup():
-	shotFile = "Basketball Data/NBAPlayerTrackingData_2014-17/2016-17_nba_shot_log.csv"
+	global shot_df
+	shotFile = dir + "game.csv"
 	shot_df = pd.read_csv(shotFile)
 
+	"""
 	possessFile = "Basketball Data/NBAPlayerTrackingData_2014-17/2016-17_nba_possession_log.csv"
 	possess_df = pd.read_csv(possessFile)
 
@@ -70,7 +95,9 @@ def startup():
 
 	svFile = "Basketball Data/NBAPlayerTrackingData_2014-17/2016-17_nba_sv_box_scores.csv"
 	sv_df = pd.read_csv(svFile)
+	"""
 
+	"""
 	print("Shot Log:")
 	for i in range(len(list(shot_df))):
 		head = list(shot_df)[i]
@@ -94,12 +121,17 @@ def startup():
 		head = list(sv_df)[l]
 		if l == 0:
 			sv_df.rename(columns={head: 'GAME_ID'}, inplace=True)
+	"""
 
+	# print(shot_df.head())
+	print(shot_df.dtypes)
 	shot_df = shot_df.sort_values(['GAME_ID', 'GAME_CLOCK'], ascending=[True, False])
+
+	"""
 	possess_df = possess_df.sort_values(['GAME_ID', 'GAME_CLOCK_START'], ascending=[True, True])
 	pass_df = pass_df.sort_values(['GAME_ID', 'GAME_CLOCK'], ascending=[True, False])
 	sv_df = sv_df.sort_values(['GAME_ID'], ascending=[True])
-
+	"""
 
 if __name__ == '__main__':
 	startup()
