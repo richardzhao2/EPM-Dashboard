2f8f2fb551312fd322ce7831694ee4bb91879814
mikinty <myou@andrew.cmu.edu>
2017-09-23 23:20:25 -0400

    installed babel presets

--- App/src/js/line.js
+++ App/src/js/line.js
@@ -2,44 +2,37 @@ const CanvasJS = require('canvasjs');
 
 var dps = []; // dataPoints
 
-		var chart = new CanvasJS.Chart("line-chart",{
-			title :{
-				text: "Live Random Data"
-			},			
-			data: [{
-				type: "line",
-				dataPoints: dps 
-			}]
-		});
+var chart = new CanvasJS.Chart('line-chart',{
+  title :{
+    text: 'Accumulating Metrics',
+  },			
+  data: [{
+    type: 'line',
+    dataPoints: dps,
+  }],
+  width: 300,
+  height: 200,
+});
 
-		var xVal = 0;
-		var yVal = 100;	
-		var updateInterval = 100;
-		var dataLength = 500; // number of dataPoints visible at any point
+var dataLength = 60; // number of dataPoints visible at any point
 
-		var updateChart = function (count) {
-			count = count || 1;
-			// count is number of times loop runs to generate random dataPoints.
-			
-			for (var j = 0; j < count; j++) {	
-				yVal = yVal +  Math.round(5 + Math.random() *(-5-5));
-				dps.push({
-					x: xVal,
-					y: yVal
-				});
-				xVal++;
-			};
-			if (dps.length > dataLength)
-			{
-				dps.shift();				
-			}
-			
-			chart.render();		
+var updateChart = function (count) {
+count = count || 1;
+// count is number of times loop runs to generate random dataPoints.
 
-		};
+for (var j = 0; j < count; j++) {	
+    var yVal = yVal +  Math.round(5 + Math.random() *(-5-5));
+    dps.push({
+      x: j,
+      y: yVal
+    });
+  };
+  if (dps.length > dataLength) {
+    dps.shift();				
+  }
+	
+  chart.render();		
+};
 
-		// generates first set of dataPoints
-		updateChart(dataLength); 
-
-		// update chart after specified time. 
-		setInterval(function(){updateChart()}, updateInterval); 
+// generates first set of dataPoints
+updateChart(30);
